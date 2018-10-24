bac885409ae3d60d67b382a3d7a9ce3c1288479e
Deiby Ordoñez Diaz <deibyod@gmail.com>
2018-10-24 14:37:09 -0300

    no-sandbox on travis

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -26,6 +26,18 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false
   });
+
+  if (process.env.TRAVIS) {
+    cfg.browsers = ['Chrome_travis_ci'];
+  }
+
+  config.set(cfg);
 };
