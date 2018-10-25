04b967e1dfb1fff9c4cb1129b9870911cec6cc31
Deiby Ordoñez Diaz <deibyod@gmail.com>
2018-10-25 14:28:10 -0300

    Cambio de nombre de app - Repo en travis

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to angular-for-ci!');
+    expect(page.getParagraphText()).toEqual('Welcome to angular-devops!');
   });
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'angular-for-ci'`, async(() => {
+  it(`should have as title 'angular-devops'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-for-ci');
+    expect(app.title).toEqual('angular-devops');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-for-ci!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-devops!');
   }));
 });
